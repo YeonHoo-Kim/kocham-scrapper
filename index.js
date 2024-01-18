f25ba42f15e1fe9e519a9204c0b3d8ef0549b7ab
YeonHoo Kim <kywho0915@naver.com>
2024-01-18 22:55:46 +0900

    [feat] puppeteer 사용하여 크롤링 구현

--- index.js
+++ index.js
@@ -5,82 +5,84 @@ const xlsx = require("xlsx");
 
 // 수정 부분
 const START_ID = 1; // 시작 id
-const END_ID = 1; // 종료 id
-const interval = 1000; // 조회 주기 (ms 단위)
+const END_ID = 5; // 종료 id
+const interval = 10000; // 조회 주기 (ms 단위)
 
 const BASE_URL = "http://www.kocham.kr/theme/inet/sub/detail.php?wr_id=";
+const HEADER = [
+  "Company name",
+  "General Director",
+  "Type of business",
+  "Tel",
+  "Area",
+  "Address",
+  "E-mail",
+  "Homepage",
+  "Number of staff",
+  "Service",
+];
 
-function start() {
-  for (let i = START_ID; i <= END_ID; i++) {
-    getTableElementById(i);
-  }
-}
-async function getTableElementById(id) {
+async function start() {
   // node 환경에서 브라우저 접속
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  const data = [HEADER];
 
+  for (let i = START_ID; i <= END_ID; i++) {
+    const row = await getRowData(page, i);
+    data.push(row);
+    printProgress(Math.round(((i - START_ID) / (END_ID - START_ID)) * 100));
+    await sleep(interval);
+  }
+
+  // 엑셀 추출
+  exportToExcel(data);
+
+  // 브라우저 종료
+  await browser.close();
+  console.log("\n종료되었습니다...");
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function printProgress(progress) {
+  process.stdout.clearLine(0);
+  process.stdout.cursorTo(0);
+  process.stdout.write("진행률: " + progress + "%");
+}
+
+async function getRowData(page, id) {
   // target 페이지 접속
   await page.goto(BASE_URL + id);
 
   // document (전체 페이지)
   const content = await page.content();
   const $ = cheerio.load(content);
-  const table = $(".subtable_list");
-  table.each((index, el) => {
-    console.log();
-  });
 
-  await browser.close();
-}
-function exportToExcel() {
-  const htmlTableString = `
-    <table>
-       <thead>
-          <tr>
-             <th>Name</th>
-             <th>Age</th>
-             <th>Country</th>
-          </tr>
-       </thead>
-       <tbody>
-          <tr>
-             <td>John Doe</td>
-             <td>25</td>
-             <td>USA</td>
-          </tr>
-          <tr>
-             <td>Jane Doe</td>
-             <td>30</td>
-             <td>Canada</td>
-          </tr>
-       </tbody>
-    </table>
- `;
-
-  const $ = cheerio.load(htmlTableString);
-
-  const data = [];
-  $("table tbody tr").each((index, row) => {
-    const rowData = [];
+  // row data 추출
+  const rowData = [];
+  $("table.subtable_list tbody tr").each((_, row) => {
     $(row)
       .find("td")
-      .each((i, cell) => {
-        rowData.push($(cell).text());
+      .each((_, cell) => {
+        rowData.push($(cell).text().trim());
       });
-    data.push(rowData);
   });
-  console.log(data);
 
+  return rowData;
+}
+function exportToExcel(data) {
   const workbook = xlsx.utils.book_new();
-  const sheetName = 'kocham';
+  const sheetName = "kocham";
 
   const worksheet = xlsx.utils.aoa_to_sheet(data);
   xlsx.utils.book_append_sheet(workbook, worksheet, sheetName);
 
-  const outputFilePath = 'kocham.xlsx';
+  const outputFilePath = "kocham.xlsx";
   xlsx.writeFile(workbook, outputFilePath);
 }
 
 // 실행
-exportToExcel();
+start();
